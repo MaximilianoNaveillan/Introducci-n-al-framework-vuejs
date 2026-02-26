9605c7180c86eee222a0221fb14d7204a6fd3f08
jsalasp01 <yooyiisalas@gmail.com>
2026-02-25 21:27:34 -0300

    add proyecto clase

--- alumnos/josseline-salas/vue-project/src/App.vue
+++ alumnos/josseline-salas/vue-project/src/App.vue
@@ -1,29 +1,16 @@
-<script setup>
-import { ref, computed } from 'vue';
-const contador = ref(0); // ref es una función que devuelve un objeto reactivo, es decir, un objeto que se actualiza automáticamente cuando se modifica su valor --> ref( valor inicial )
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
-const tareas = ref(['Aprender Vue', 'Practicar componentes', 'Construir proyectos']);
+<div id="app">{{ message }}</div>
 
-const frameworks = ref(['Vue', 'Rect', 'Angular']);
+<script>
+const { createApp, ref } = Vue;
 
-function incrementar() {
-  console.log(contador);
-  contador.value++; // para acceder al valor de un ref, se utiliza la propiedad value
-  // Propiedades Computadas: Son funciones que se ejecutan automáticamente cuando se accede a ellas y devuelven un valor calculado en función de otras propiedades reactivas. Se utilizan para realizar cálculos o transformaciones de datos sin necesidad de modificar el estado original.
-}
-
-const totalFrameworks = computed(() => frameworks.value.length);
+createApp({
+  setup() {
+    const message = ref('¡Hola Vue!');
+    return {
+      message,
+    };
+  },
+}).mount('#app');
 </script>
-
-<template>
-  <button @click="incrementar">Clicks : {{ contador }}</button>
-  <ul>
-    <li v-for="(tarea, index) in tareas" :key="index">{{ tarea }}</li>
-  </ul>
-  <ul>
-    <li v-for="(framework, index) in frameworks">{{ framework }}</li>
-  </ul>
-  <p>Total Frameworks: {{ totalFrameworks }}</p>
-</template>
-
-<style scoped></style>
